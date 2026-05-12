21a38fdbc3944df8b57269ac79f0763b6df96af6
srinidhicr <21pt30@psgtech.ac.in>
2026-05-12 10:28:28 +0530

    Remove local files

--- hf.py
+++
@@ -1,5 +0,0 @@
-from sentence_transformers import SentenceTransformer, CrossEncoder
-
-# Download and cache
-SentenceTransformer("all-MiniLM-L6-v2")
-CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
